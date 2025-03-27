24921c833816143abb04a9c119892fc34e302040
Gigena07 <gigena2007@gmail.com>
2025-03-27 15:35:11 -0300

    error de variables

---
+++ constante.php
@@ -0,0 +1,10 @@
+<?php 
+const pi=3.14;
+const pi=3.1416;
+/*que error arrojo PHP al intentar cambiar el valor de la constante?
+que nos indica este error sobre el comportamiento de las constantes en PHP?
+en que situacion  seria util utilizar constantes en lugar de variables ?
+1R:Warning: Constant pi already defined in C:\xampp\htdocs\programaci-nweb2025\constante.php on line 3
+2R:que la variable ya avia sido definida es decir q havia  dos variables iguales 
+3R:nops sirve pqara guardar un valor q luego sea mas facil a la hora de la modificasion
+?>
--- hola.php
+++ hola.php
@@ -2,22 +2,19 @@
 echo "hola perreque";
 
 //todo bien ?
-/* estamos re bien 
-<h1>Hola mundo</h1>
-*/
-# hola 
-$edad = 18;
-$nombre_de_cliente="benja"
-$nombre ='manolo';// texto. puede ser con comillas simples o doblñes(string)
-$edad = 31; // enteros (integer)
-$altura= 1.72;// decimales, usando el punto en el lugar de la coma (float)
-$mayorEdad = true; //verdadero o falso (boolean)
+
+
 //trabajo
-$nombreCompleto="juan perez" //esta todo bien
+$nombreCompleto="juan perez"; //esta todo bien
 $Edad = 25; // esta todo ok
 $estAtura=1.75;// ok
 $numero="10";// faltaria a mi parecesr especificar numero de q porque la variableporque el nombre numero es muy apmlio y puede almacenar todo tipo de datos 
-apellido='fernandez';// no uliza signoi de pesosn el arancar la bariable luego pone comillas simples enves de dobles
-pais_de_oruigen="Uruguay"// lo mismo la variable no empienza con signo de pesos y no termina con punto y coma
-$salario=$2000;// utiliza nuebamente el signo de pesos  para marcar el salario a lo cual el 2000  se llamaria es decir q salario se pasa allamar 2000 
+$apellido='fernandez';// no uliza signoi de pesosn el arancar la bariable luego pone comillas simples enves de dobles
+
+$pais_de_oruigen="Uruguay";// lo mismo la variable no empienza con signo de pesos y no termina con punto y coma
+$Salario = 2000;// utiliza nuebamente el signo de pesos  para marcar el salario a lo cual el 2000  se llamaria es decir q salario se pasa allamar 2000 
+
+define('GRAVEDAD',9.8);
+const GRAVEDAD =9.8;
+
 ?> 
